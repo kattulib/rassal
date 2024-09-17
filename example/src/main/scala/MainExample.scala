adc8d7f6aa98b0d97a77dfb05ab5cd9b46e9edcc
csgn <dev.csgn@gmail.com>
2024-09-17 14:59:27 +0300

    feat(example): add example1

--- example/src/main/scala/MainExample.scala
+++ example/src/main/scala/MainExample.scala
@@ -1,7 +1,32 @@
-object MainExample {
-  @main def example1: Unit =
-    def solve(a: Int, b: Int): Int = 
-      a + b
+/*
+ * Copyright (c) 2024 kattulib
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
-    assert(solve(1, 2) == 3, "wrong!")
+import rassal.{Gen, Seed}
+import rassal.syntax.all.*
+import rassal.instances.all.given
+
+object MainExample {
+  @main def example1: Unit = {
+    val d = Gen.nextDouble.invert.toFixed(2)
+
+    println(d.run(Seed(1)))
+  }
 }
